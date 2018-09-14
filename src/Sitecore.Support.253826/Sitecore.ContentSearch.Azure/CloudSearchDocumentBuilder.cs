2daa73521a54646532d1e56a0a46047ca2ea322f
DK\tpe <tpe@sitecore.net>
2018-09-14 10:48:51 +0300

    Importing initial code of the CloudSearchDocumentBuilder.AddField() method

---
+++ src/Sitecore.Support.253826/Sitecore.ContentSearch.Azure/CloudSearchDocumentBuilder.cs
@@ -0,0 +1,36 @@
+﻿namespace Sitecore.Support.ContentSearch.Azure
+{
+  using Sitecore.ContentSearch;
+  using Sitecore.ContentSearch.Azure;
+  using Sitecore.ContentSearch.Diagnostics;
+
+  public class CloudSearchDocumentBuilder : Sitecore.ContentSearch.Azure.CloudSearchDocumentBuilder
+  {
+    public CloudSearchDocumentBuilder(IIndexable indexable, IProviderUpdateContext context)
+           : base(indexable, context)
+    {}
+
+    protected override void AddField(IIndexableDataField field)
+    {
+      if (string.IsNullOrEmpty(field.Name))
+      {
+        CrawlingLog.Log.Warn($"[Index={this.Index.Name}] '{field.Id}' field of '{this.Indexable.Id}' item is skipped: the field name is missed.");
+        return;
+      }
+
+      var fieldName = field.Name;
+      var fieldValue = this.Index.Configuration.FieldReaders.GetFieldValue(field);
+
+      var indexConfiguration = this.Index.Configuration.FieldMap.GetFieldConfiguration(field);
+      var cloudConfiguration = indexConfiguration as CloudSearchFieldConfiguration;
+
+      if (this.IsTextField(field) && this.IsFieldSearchable(cloudConfiguration))
+      {
+        this.AddField(BuiltinFields.Content, fieldValue, true);
+      }
+
+      // Based on SMAR's comments in issue 96467, the append must be false.
+      this.AddField(fieldName, fieldValue, cloudConfiguration, false);
+    }
+  }
+}
