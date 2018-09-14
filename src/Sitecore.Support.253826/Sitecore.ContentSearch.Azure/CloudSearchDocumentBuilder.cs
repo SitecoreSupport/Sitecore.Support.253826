75bd5dd554a0d79223f38eeca9a27d67f833c050
DK\tpe <tpe@sitecore.net>
2018-09-14 12:29:35 +0300

    Add if clause to check that fieldname is not empty

--- src/Sitecore.Support.253826/Sitecore.ContentSearch.Azure/CloudSearchDocumentBuilder.cs
+++ src/Sitecore.Support.253826/Sitecore.ContentSearch.Azure/CloudSearchDocumentBuilder.cs
@@ -12,19 +12,22 @@
 
     protected override void AddField(IIndexableDataField field)
     {
-      var fieldName = field.Name;
-      var fieldValue = this.Index.Configuration.FieldReaders.GetFieldValue(field);
-
-      var indexConfiguration = this.Index.Configuration.FieldMap.GetFieldConfiguration(field);
-      var cloudConfiguration = indexConfiguration as CloudSearchFieldConfiguration;
-
-      if (this.IsTextField(field) && this.IsFieldSearchable(cloudConfiguration))
+      if (!string.IsNullOrEmpty(field.Name))
       {
-        this.AddField(BuiltinFields.Content, fieldValue, true);
-      }
+        var fieldName = field.Name;
+        var fieldValue = this.Index.Configuration.FieldReaders.GetFieldValue(field);
 
-      // Based on SMAR's comments in issue 96467, the append must be false.
-      this.AddField(fieldName, fieldValue, cloudConfiguration, false);
+        var indexConfiguration = this.Index.Configuration.FieldMap.GetFieldConfiguration(field);
+        var cloudConfiguration = indexConfiguration as CloudSearchFieldConfiguration;
+
+        if (this.IsTextField(field) && this.IsFieldSearchable(cloudConfiguration))
+        {
+          this.AddField(BuiltinFields.Content, fieldValue, true);
+        }
+
+        // Based on SMAR's comments in issue 96467, the append must be false.
+        this.AddField(fieldName, fieldValue, cloudConfiguration, false);
+      }
     }
   }
 }
